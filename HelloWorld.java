99ca92f94a7e12e32e807d57618fcaf2599489f3
root <root@DESKTOP-K69L5V6>
2024-11-30 19:54:11 -0500

    Uploadnig files

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+    // System.out.println("Hello World");
+    int myNum = 5;
+    float myFloatNum = 5.99f;
+    char myLetter = 'D';
+    boolean myBool = true;
+    String myText = "Hello";
+    System.out.println(myText);
+    System.out.println(myNum);
+    System.out.println(myFloatNum);
+    System.out.println(myLetter);
+    System.out.println(myBool);
+    System.out.println(myText);
+  }
+  
+}
+
